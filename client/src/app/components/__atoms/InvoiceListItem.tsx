7d909d45b7f223988f1f74bf8c97b3dd6b08f0be
nikaxucishvil1 <xucishvilin90@gmail.com>
2024-10-27 18:35:53 +0400

    done list

---
+++ client/src/app/components/__atoms/InvoiceListItem.tsx
@@ -0,0 +1,40 @@
+interface InvoiceListItemIF {
+  invoice: Invoice;
+}
+const InvoiceListItem = (props: InvoiceListItemIF) => {
+  const { invoice } = props;
+  return (
+    <div className="w-full bg-[#FFFF] p-4 flex rounded-lg flex-col gap-4 justify-center">
+      <header className="flex items-center justify-between w-full">
+        <h1 className="text-[#0C0E16] text-[15px] font-bold">
+          #{invoice._id.slice(6, 12)}
+        </h1>
+        <h1 className="text-[#858BB2] text-[13px] font-[500]">
+          {invoice.billTo.clientName}
+        </h1>
+      </header>
+      <footer className="flex items-center justify-between">
+        <div>
+          <h1 className="text-[13px] text-[#888EB0] font-[500]">
+            {invoice.invoiceDate}
+          </h1>
+          <p className="text-[#0C0E16] text-[15px] font-bold">
+            $ {invoice.itemList.reduce((acc, curr) => acc + curr.total, 0)}
+          </p>
+        </div>
+        <div
+          style={{
+            backgroundColor:
+              invoice.status === "Pending" ? "#FF8F000F" : "#33D69F0F",
+            color: invoice.status === "Pending" ? "#FF8F00" : "#33D69F",
+          }}
+          className="rounded-md p-4 text-[15px] font-bold"
+        >
+          <p> {invoice.status}</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default InvoiceListItem;
--- client/src/app/components/__molecules/InvoiceList.tsx
+++ client/src/app/components/__molecules/InvoiceList.tsx
@@ -1,25 +1,37 @@
 "use client";
+import { FaPlus } from "react-icons/fa6";
+import InvoiceListItem from "../__atoms/InvoiceListItem";
 
 interface invoiceList {
   invoices: Invoice[];
 }
+
 const InvoiceList = (props: invoiceList) => {
   const { invoices } = props;
   return (
-    <div>
-      <header>
-        <div>
-          <h1>Invoices</h1>
-          <p>{invoices.length} invoices</p>
+    <div className="bg-[#F8F8FB] min-h-screen">
+      <header className="flex p-4 items-center justify-between">
+        <div className="flex flex-col">
+          <h1 className="text-[24px] text-[#0C0E16] font-bold">Invoices</h1>
+          <p className="text-[#888EB0] text-[13px] font-[500]">
+            {invoices.length} invoices
+          </p>
         </div>
-        <div>
-          <h1>Filter</h1>
-          <button>New</button>
+        <div className="flex items-center gap-5">
+          <h1 className="text-[15px] font-bold text-[#0C0E16]">Filter</h1>
+          <button className="rounded-[24px] bg-[#7C5DFA] flex items-center justify-center gap-4 p-2 pr-4">
+            <div className="bg-[#FFFF] p-2 rounded-full">
+              <FaPlus color="#7C5DFA" />
+            </div>
+            <p className="text-[#FFFFFF] font-bold text-[15px]">New</p>
+          </button>
         </div>
       </header>
-      {invoices.map((el) => (
-        <div></div>
-      ))}
+      <div className="p-7 flex items-center justify-center flex-col gap-5">
+        {invoices.map((el) => (
+          <InvoiceListItem invoice={el} />
+        ))}
+      </div>
     </div>
   );
 };
